07c40066a3d466e46920f2e65ee97b57c4b37a25
sotirr <sotirr@gmail.com>
2021-01-31 21:52:04 +0500

    Docker work!

--- test_task/test_task/settings.py
+++ test_task/test_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5acofnrqhgw4700o^+hi9=*+113#%a)lnl!*go!&ecjm6gm)%z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
